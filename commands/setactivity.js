fea287a9e7aa699d32a739a021ce8c951b48aaaf
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-24 21:27:53 -0500

    Update setactivity.js

--- commands/setactivity.js
+++ commands/setactivity.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 let tbh = args.join(" ").toUpperCase()
 
-if(message.author.id === "291367352476631040") {
+if(message.author.id === "303683211790254080") {
 bot.user.setActivity(`${bot.user.presence.game.name}`, {type: `${tbh}`});
       message.react("\u2705")
 } else if(message.author.id === "245877990938902529") {
